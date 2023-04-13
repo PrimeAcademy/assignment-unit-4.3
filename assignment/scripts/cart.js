b4791b0b9d0085264015fda719181b16a51eed38
Luke Gartner <lukegartner4@gmail.com>
2023-04-12 20:00:13 -0500

    stretch goals complete

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,10 +3,15 @@ console.log("***** Cart Functions *****");
 // We want to see how you are testing your code!!!
 
 let basket = [];
+const maxItems = 5;
 
 function addItem(item) {
-  basket.push(item);
-  return true;
+  if (basket.length < maxItems) {
+    basket.push(item);
+    return true;
+  } else if (basket.length >= maxItems) {
+    return false;
+  }
 }
 
 // test
@@ -21,3 +26,46 @@ function listItems(basketItems) {
   }
 }
 listItems(basket);
+
+// empty
+function empty() {
+  basket = [];
+}
+empty();
+console.log("empty basket:", basket);
+// add items
+addItem("pants");
+// Sretch Goals
+
+// function isFull(){};
+const isFull = () => {
+  if (basket.length < maxItems) {
+    return false;
+  } else return true;
+};
+
+console.log("Basket", basket, "Max items: 5 is it full", isFull());
+
+addItem("socks");
+addItem("shirt");
+addItem("bandana");
+
+console.log("Basket", basket, "Max items: 5 is it full", isFull());
+
+addItem("jacket");
+console.log(addItem("hat"));
+console.log(basket);
+
+// Remove Item
+function removeItem(item) {
+  let index = basket.indexOf(item);
+
+  if (index !== -1) {
+    basket.splice(index, 1);
+    return item;
+  }
+  return null;
+}
+
+console.log("remove: ", removeItem("pants"), "new basket: ", basket);
+console.log("remove: ", removeItem("blouse"), "new basket: ", basket);
