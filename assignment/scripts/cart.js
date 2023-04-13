063a6b6ada71a201f8d62fbb7b495ee653e9e5ad
Luke Gartner <lukegartner4@gmail.com>
2023-04-12 19:12:58 -0500

    required complete

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,23 @@
-console.log('***** Cart Functions *****');
+console.log("***** Cart Functions *****");
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+function addItem(item) {
+  basket.push(item);
+  return true;
+}
+
+// test
+addItem("shoes");
+console.log("added shoes, basket:", basket);
+addItem("pants");
+console.log("added pants, basket:", basket);
+
+function listItems(basketItems) {
+  for (let item of basketItems) {
+    console.log(item);
+  }
+}
+listItems(basket);
